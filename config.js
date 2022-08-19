b55db38010d895dbe62865207f7bb1edb3a3765f
Mabakalari <86472563+Mabakalari@users.noreply.github.com>
2022-08-19 12:17:36 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: "",
+    channel: "",
+    messageTemplate: "Slaw @everyone, **{author}** Vedio nwe bllawkrayawa✅ **{title}**!\n{url}",
+    channel_id: "UCFbWvTkRe8Fq2aXmnIvLp9A",
+    watchInterval: 30000
